60343474a261eb5f098d4b7ae9018e2feb1547bc
Yanis <80279403+12-3-8-s9b9o9j9t@users.noreply.github.com>
2021-03-08 19:28:34 +0100

    Update index.svelte

--- src/routes/?s9b9o9j9t/[name]/index.svelte
+++ src/routes/?s9b9o9j9t/[name]/index.svelte
@@ -1,1 +1,41 @@
+<style>
+
+    img {
+        animation: rotate linear 3s infinite;
+        height: 50%;
+    }
+
+
+
+    img:hover {
+        cursor: pointer;
+    }
+
+    main {
+        overflow: hidden;
+        height: 100vh;
+        width: 100%;
+        background-color: #f1ff59;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+
+</style>
+
+<script lang="ts">
+let isHovered = false;
+$: path = isHovered ? "cody-o.png" : "cody-d.png";
+
+
+</script>
+
+<svelte:head>
+  <title>Cody le troll</title>
+</svelte:head>
+
+<main>
+  <img on:mouseenter={() => {isHovered = true}} on:mouseleave={() => isHovered = false} src="{path}" alt="cody">
+</main>
 
