32313e3c92f970a0f62135c5adeaab7d12923eea
Yanis <80279403+12-3-8-s9b9o9j9t@users.noreply.github.com>
2021-03-08 19:31:05 +0100

    Update index.svelte

--- src/routes/?s9b9o9j9t/[name]/index.svelte
+++ src/routes/?s9b9o9j9t/[name]/index.svelte
@@ -1,41 +1,33 @@
-<style>
-
-    img {
-        animation: rotate linear 3s infinite;
-        height: 50%;
-    }
-
-
-
-    img:hover {
-        cursor: pointer;
-    }
-
-    main {
-        overflow: hidden;
-        height: 100vh;
-        width: 100%;
-        background-color: #f1ff59;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-
-
-</style>
+<script lang="ts" context="module">
+export async function preload(page) {
+  const { name }: { name: string } = page.params;
+  return { name: name.charAt(0).toUpperCase() + name.slice(1) };
+}
+</script>
 
 <script lang="ts">
-let isHovered = false;
-$: path = isHovered ? "cody-o.png" : "cody-d.png";
+import TextCustom from "../../components/TextCustom.svelte";
+import Room from "../../components/Room.svelte";
+import RotatingAvatar from "../../components/RotatingAvatar.svelte";
 
-
+export let name: string;
 </script>
 
 <svelte:head>
-  <title>Cody le troll</title>
+  <title>{name} a quelque chose à vous dire</title>
+  <meta name="title" content="{name} a quelque chose à vous dire">
+  <meta name="description"
+        content="Hey ! C'est moi {name}. J'ai vraiment envie de te dire quelque chose... Mais je t'avoue que je ne sais pas trop comment m'y prendre. ❤😘😍💋">
+  <meta property="og:title" content="{name} a quelque chose à vous dire">
+  <meta property="og:description"
+        content="Hey ! C'est moi {name}. J'ai vraiment envie de te dire quelque chose... Mais je t'avoue que je ne sais pas trop comment m'y prendre. ❤😘😍💋">
+  <meta property="twitter:title" content="{name} a quelque chose à vous dire">
+  <meta property="twitter:description"
+        content="Hey ! C'est moi {name}. J'ai vraiment envie de te dire quelque chose... Mais je t'avoue que je ne sais pas trop comment m'y prendre. ❤😘😍💋">
 </svelte:head>
 
-<main>
-  <img on:mouseenter={() => {isHovered = true}} on:mouseleave={() => isHovered = false} src="{path}" alt="cody">
-</main>
-
+<Room>
+  <TextCustom color="#00F7FF" x={1} y={1} size="5rem" move="-2rem">{name} vous a Codysé !</TextCustom>
+  <TextCustom color="#77FF00" x={0.5} y={0.5} size="2rem" move="2rem">Gros bisous sur tes joues mon loulou</TextCustom>
+  <RotatingAvatar/>
+</Room>
