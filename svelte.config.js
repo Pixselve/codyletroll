32f17e1e4bb9dc15819964f258469d3c171335c7
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-06-27 12:17:14 +0200

    :sparkles: deploy to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +12,7 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
     target: '#svelte'
   }
 };
